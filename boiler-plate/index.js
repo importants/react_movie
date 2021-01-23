81b041bbe987c7e1a62bdfb0acfa3f2950dff99a
ljh9716 <ljhjohn1@gmail.com>
2021-01-23 23:28:34 +0900

    아이디 토큰 가져오기

--- boiler-plate/index.js
+++ boiler-plate/index.js
@@ -5,8 +5,8 @@ const app = express(); // 앱을 만든다
 const port = 5000; // 포트 생성
 const bodyParser = require(`body-parser`);
 const config = require(`./config/key`);
-
 const { User } = require("./models/User");
+const cookieParser = require(`cookie-parser`);
 
 //appication/x-www-form-urlencoded 데이터를 가져오는 것
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -14,10 +14,12 @@ app.use(bodyParser.urlencoded({ extended: true }));
 //appication/json 데이터 가져오는 것
 app.use(bodyParser.json());
 
+app.use(cookieParser());
+
 const mongoose = require(`mongoose`);
 mongoose
   .connect(
-    ``,
+    config.mongoURI,
     {
       useNewUrlParser: true,
       useUnifiedTopology: true,
@@ -43,12 +45,47 @@ app.post("/register", (req, res) => {
   // }
   const user = new User(req.body);
 
+  // save 하기 전에 암호화를 먼저 해줘야 한다
   user.save((err, userInfo) => {
     if (err) return res.json({ success: false, err });
     return res.status(200).json({
       success: true,
     });
   });
+});
+
+app.post(`/login`, (req, res) => {
+  // 요청된 이메일을 데이터베이스에서 있는 지 찾는다.
+  User.findOne({ email: req.body.email }, (err, user) => {
+    // 요청된 이메일을 findOne에서 찾고 그것에 대한 응답을 json 형식으로 보낸다.
+    if (!user) {
+      // email이 확인이 안됨
+      return res.json({
+        loginSuccess: false,
+        message: "제공된 이메일에 해당하는 유저가 없습니다",
+      });
+    }
+    //요청된 이메일이 데이터 베이스에 있다면 비밀번호가 맞는 비밀번호 인지 확인
+    user.comparePassword(req.body.password, (err, isMatch) => {
+      // comparePassword에 사용자가 입력한 패스워드를 User.js 에 옮겨짐 callback함수로부터 나온 err,isMatch값이 2번째 인자로 나옴
+
+      if (!isMatch)
+        return res.json({
+          loginSuccess: false,
+          message: "비밀번호가 틀렸습니다",
+        });
+
+      user.generateToken((err, user) => {
+        // user 안에 토큰이 들어있다 토큰은 쿠키에 or 로컬스토리지에 저장 -> 쿠키를 깔자 cookie-parser
+        if (err) return res.status(400).send(err);
+
+        res.cookie(`x_auth`); // x_auth 쿠키에 토큰이 저장
+      }); //함수이름 바꿔도 됨
+    });
+  });
+  //요청된 이메일이 데이터 베이스에 있다면 비밀번호가 맞는 비밀번호 인지 확인
+
+  // 비밀번호 까지 맞다면 토큰을 생성하기
 });
 
 app.listen(port, () => {
@@ -59,3 +96,6 @@ app.listen(port, () => {
 //yarn add body-parser --save
 //postman 설치
 // node mon == pm2 ->yarn add nodemon --save-dev
+// 비밀번호 노출 안되게 하기 위해 yarn add bcrypt --save
+// 비밀번호 맞다면 유저의 토큰을 생성해야한다 yarn add jsonwebtoken --save
+//yarn add cookie-parser --save
--- boiler-plate/models/User.js
+++ boiler-plate/models/User.js
@@ -1,4 +1,7 @@
 const mongoose = require(`mongoose`);
+const bcrypt = require(`bcrypt`);
+const saltRounds = 10; // 몇 자리 솔트를 만들 건지
+const jwt = require(`jsonwebtoken`);
 
 const userSchema = mongoose.Schema({
   name: {
@@ -28,6 +31,49 @@ const userSchema = mongoose.Schema({
   }, //유용성
   tokenExp: { type: Number },
 });
+
+// 유저 정보를 저장하기 전에 무언가를 한다
+userSchema.pre(`save`, function (next) {
+  //-> 만약 이렇게만 쓰게 된다면 수정할 때도 암호화가 된다
+  // 비밀번호 암호화 1. 솔트 만들기 2.
+  var user = this;
+  bcrypt.genSalt(saltRounds, function (err, salt) {
+    if (err) return next(err);
+    if (user.isModified("password")) {
+      bcrypt.hash(user.password, salt, function (err, hash) {
+        // 첫번째 인자에 비밀번호
+        if (err) return next(err);
+        user.password = hash; // 바꿔주기
+        next(); // 끝나면 next로 보내버림 -> save로 보냄
+      });
+    } else {
+      next();
+    }
+  });
+});
+
+userSchema.methods.comparePassword = function (plainPassword, callback) {
+  //1번째 인자에 사용자가 로그인 할 때 입력한 비밀번호
+  //plainPassword  === 암호화된 비밀번호 ??
+  bcrypt.compare(plainPassword, this.password, function (err, isMatch) {
+    if (err) return callback(err);
+    callback(null, isMatch); // 비밀번호가 같다면  err == null / isMatch값은 true => user.comparePassword의 isMatch값으로 이동
+  });
+};
+
+userSchema.methods.generateToken = function (callback) {
+  //jsonwebtoken 이용해서 token을 생성하기
+  let user = this;
+  let token = jwt.sign(user._id, `secretToken`);
+
+  user.token = token;
+  user.save(function (err, user) {
+    if (err) return callback(err);
+    callback(null, user);
+  });
+
+  //user._id + `secretToken` = token -> 이 token을 가지고 id를 알 수 있다
+};
 
 const User = mongoose.model(`User`, userSchema); // 모델의 모델 User-> 스키마 UserSchema
 
